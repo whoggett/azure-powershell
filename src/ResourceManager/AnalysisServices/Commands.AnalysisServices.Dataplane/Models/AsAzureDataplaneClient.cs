c8e5162ab0cde5f661c8851dca6df7a242b41bbe
Gabriel Prestemon <gapreste@microsoft.com>
2018-12-19 13:45:15 -0800

    Use login credentials from new Az module.
    Remove username/password authentication.
    Get login credentials from Az module, instead of AddAzureASAccount.
    
    This is currently a work in progress.

---
+++ src/ResourceManager/AnalysisServices/Commands.AnalysisServices.Dataplane/Models/AsAzureDataplaneClient.cs
@@ -0,0 +1,115 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Rest;
+
+namespace Microsoft.Azure.Commands.AnalysisServices.Dataplane.Models
+{
+    /// <summary>
+    /// Contains properties and logic for <see cref="AsAzureDataplaneClient"/>.
+    /// All permutations of constructors, CallGetAsync, and CallPostAsync are in the partial class in order to make this partial class more readable.
+    /// </summary>
+    /// <remarks>Most modifications will likely be in this part of the class.</remarks>
+    public partial class AsAzureDataplaneClient : ServiceClient<AsAzureDataplaneClient>, IAsAzureHttpClient
+    {
+        /// <summary>
+        /// The base Uri of the service.
+        /// </summary>
+        public Uri BaseUri { get; set; }
+
+        /// <summary>
+        /// Credentials needed for the client to connect to Azure.
+        /// </summary>
+        public ServiceClientCredentials Credentials { get; private set; }
+
+        private Func<HttpClient> HttpClientProvider { get; set; }
+
+        // TODO: This might not work, but is only solution for multiple inhertiance
+        public new HttpClient HttpClient { get; set; }
+
+        public void resetHttpClient()
+        {
+            this.HttpClient = this.HttpClientProvider();
+        }
+
+        private async Task<HttpResponseMessage> SendRequestAsync(
+            HttpMethod method,
+            Uri baseUri,
+            string requestUrl,
+            Guid correlationId,
+            HttpContent content = null,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Construct URL
+            ////List<string> queryParameters = new List<string>();
+
+            // Create HTTP transport objects
+            HttpRequestMessage httpRequest = new HttpRequestMessage()
+            {
+                Method = method,
+                RequestUri = new Uri(baseUri, requestUrl)
+            };
+
+            httpRequest.Content = content;
+
+            // Set Headers
+            AddHeader(httpRequest.Headers, "x-ms-client-request-id", correlationId.ToString());
+
+            // TODO: Where is this flag located?
+            ////if (this.Client.AcceptLanguage != null)
+            ////{
+            ////    AddHeader(httpRequest.Headers, "accept-language", this.Client.AcceptLanguage);
+            ////}
+
+            // TODO: Where are the custom headers coming from?
+            ////if (customHeaders != null)
+            ////{
+            ////    foreach (var header in customHeaders)
+            ////    {
+            ////        AddHeader(httpRequest.Headers, header.Key, header.Value);
+            ////    }
+            ////}
+
+            // Set Credentials
+            if (Credentials != null)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                await Credentials.ProcessHttpRequestAsync(httpRequest, cancellationToken).ConfigureAwait(false);
+            }
+
+            HttpResponseMessage httpResponse = await this.HttpClient.SendAsync(httpRequest, cancellationToken).ConfigureAwait(false);
+            return httpResponse;
+        }
+
+        /// <summary>
+        /// Adds a header to the <see cref="HttpRequestHeaders"/> list object.
+        /// </summary>
+        /// <param name="headers">The request headers list object.</param>
+        /// <param name="name">The name of the header to add.</param>
+        /// <param name="value">The value of the header.</param>
+        private static void AddHeader(HttpRequestHeaders headers, string name, string value)
+        {
+            if (headers.Contains(name))
+            {
+                headers.Remove(name);
+            }
+            headers.TryAddWithoutValidation(name, value);
+        }
+    }
+}
---
+++ src/ResourceManager/AnalysisServices/Commands.AnalysisServices.Dataplane/Models/AsAzureDataplaneClientOverloads.cs
@@ -0,0 +1,207 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Rest;
+
+namespace Microsoft.Azure.Commands.AnalysisServices.Dataplane.Models
+{
+    /// <summary>
+    /// Contains all permutations of constructors, CallGetAsync, and CallPostAsync for <see cref="AsAzureDataplaneClient"/>.
+    /// The reside here, in order to make the other partial class more readable and easier to debug.
+    /// </summary>
+    /// <remarks>Most modifications will likely be in the other part of the class.</remarks>
+    public partial class AsAzureDataplaneClient : ServiceClient<AsAzureDataplaneClient>, IAsAzureHttpClient
+    {
+        public AsAzureDataplaneClient(params DelegatingHandler[] handlers) : base(handlers)
+        {
+            this.resetHttpClient();
+        }
+
+        public AsAzureDataplaneClient(HttpClientHandler rootHandler, params DelegatingHandler[] handlers) : base(rootHandler, handlers)
+        {
+            this.resetHttpClient();
+        }
+
+        public AsAzureDataplaneClient(Uri baseUri, params DelegatingHandler[] handlers) : this(handlers)
+        {
+            this.BaseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+        }
+
+        public AsAzureDataplaneClient(Uri baseUri, HttpClientHandler rootHandler, params DelegatingHandler[] handlers) : this(rootHandler, handlers)
+        {
+            this.BaseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+        }
+
+        public AsAzureDataplaneClient(ServiceClientCredentials credentials, params DelegatingHandler[] handlers) : this(handlers)
+        {
+            this.Credentials = credentials ?? throw new ArgumentNullException(nameof(credentials));
+            this.Credentials.InitializeServiceClient(this);
+        }
+
+        public AsAzureDataplaneClient(ServiceClientCredentials credentials, HttpClientHandler rootHandler, params DelegatingHandler[] handlers) : this(rootHandler, handlers)
+        {
+            this.Credentials = credentials ?? throw new ArgumentNullException(nameof(credentials));
+            this.Credentials.InitializeServiceClient(this);
+        }
+
+        public AsAzureDataplaneClient(Func<HttpClient> httpClientProvider, params DelegatingHandler[] handlers) : this(handlers)
+        {
+            this.HttpClientProvider = httpClientProvider ?? throw new ArgumentNullException(nameof(httpClientProvider));
+        }
+
+        public AsAzureDataplaneClient(Func<HttpClient> httpClientProvider, HttpClientHandler rootHandler, params DelegatingHandler[] handlers) : this(rootHandler, handlers)
+        {
+            this.HttpClientProvider = httpClientProvider ?? throw new ArgumentNullException(nameof(httpClientProvider));
+        }
+
+        public AsAzureDataplaneClient(Uri baseUri, ServiceClientCredentials credentials, params DelegatingHandler[] handlers) : this(credentials, handlers)
+        {
+            this.BaseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+        }
+
+        public AsAzureDataplaneClient(Uri baseUri, ServiceClientCredentials credentials, HttpClientHandler rootHandler, params DelegatingHandler[] handlers) : this(credentials, rootHandler, handlers)
+        {
+            this.BaseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+        }
+
+        public AsAzureDataplaneClient(Uri baseUri, Func<HttpClient> httpClientProvider, params DelegatingHandler[] handlers) : this(httpClientProvider, handlers)
+        {
+            this.BaseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+        }
+
+        public AsAzureDataplaneClient(Uri baseUri, Func<HttpClient> httpClientProvider, HttpClientHandler rootHandler, params DelegatingHandler[] handlers) : this(httpClientProvider, rootHandler, handlers)
+        {
+            this.BaseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+        }
+
+        public AsAzureDataplaneClient(ServiceClientCredentials credentials, Func<HttpClient> httpClientProvider, params DelegatingHandler[] handlers) : this(credentials, handlers)
+        {
+            this.HttpClientProvider = httpClientProvider ?? throw new ArgumentNullException(nameof(httpClientProvider));
+        }
+
+        public AsAzureDataplaneClient(ServiceClientCredentials credentials, Func<HttpClient> httpClientProvider, HttpClientHandler rootHandler, params DelegatingHandler[] handlers) : this(credentials, rootHandler, handlers)
+        {
+            this.HttpClientProvider = httpClientProvider ?? throw new ArgumentNullException(nameof(httpClientProvider));
+        }
+
+        public AsAzureDataplaneClient(Uri baseUri, ServiceClientCredentials credentials, Func<HttpClient> httpClientProvider, params DelegatingHandler[] handlers) : this(baseUri, credentials, handlers)
+        {
+            this.HttpClientProvider = httpClientProvider ?? throw new ArgumentNullException(nameof(httpClientProvider));
+        }
+
+        public AsAzureDataplaneClient(Uri baseUri, ServiceClientCredentials credentials, Func<HttpClient> httpClientProvider, HttpClientHandler rootHandler, params DelegatingHandler[] handlers) : this(baseUri, credentials, rootHandler, handlers)
+        {
+            this.HttpClientProvider = httpClientProvider ?? throw new ArgumentNullException(nameof(httpClientProvider));
+        }
+
+        /// <summary>
+        /// Calls SendRequestAsync() for a GET using a blank correlationId.
+        /// </summary>
+        /// <param name="baseUri">The base Uri to call.</param>
+        /// <param name="requestUrl">The request Url.</param>
+        /// <param name="accessToken">The access token (will be discarded).</param>
+        /// <returns>The http response message.</returns>
+        public async Task<HttpResponseMessage> CallGetAsync(Uri baseUri, string requestUrl, string accessToken)
+        {
+            return await CallGetAsync(baseUri: baseUri, requestUrl: requestUrl, accessToken: accessToken, correlationId: new Guid());
+        }
+
+        /// <summary>
+        /// Calls SendRequestAsync() for a GET.
+        /// </summary>
+        /// <param name="baseUri">The base Uri to call.</param>
+        /// <param name="requestUrl">The request Url.</param>
+        /// <param name="accessToken">The access token (will be discarded).</param>
+        /// <param name="correlationId">The CorrelationId</param>
+        /// <returns>The http response message.</returns>
+        public async Task<HttpResponseMessage> CallGetAsync(Uri baseUri, string requestUrl, string accessToken = null, Guid correlationId = new Guid())
+        {
+            return await SendRequestAsync(HttpMethod.Get, baseUri: baseUri, requestUrl: requestUrl, correlationId: correlationId);
+        }
+
+        /// <summary>
+        /// Calls SendRequestAsync() for a GET using the default BaseUri and a blank correlationId.
+        /// </summary>
+        /// <param name="requestUrl">The Request Url.</param>
+        /// <returns>The http response message.</returns>
+        public async Task<HttpResponseMessage> CallGetAsync(string requestUrl)
+        {
+            return await CallGetAsync(requestUrl, new Guid());
+        }
+
+        /// <summary>
+        /// Calls SendRequestAsync() for a GET using the default BaseUri.
+        /// </summary>
+        /// <param name="requestUrl">The Request Url.</param>
+        /// <param name="correlationId">The CorrelationId</param>
+        /// <returns>The http response message.</returns>
+        public async Task<HttpResponseMessage> CallGetAsync(string requestUrl, Guid correlationId)
+        {
+            return await SendRequestAsync(HttpMethod.Get, BaseUri, requestUrl, correlationId);
+        }
+
+        /// <summary>
+        /// Calls SendRequestAsync() for a POST using a blank correlationId.
+        /// </summary>
+        /// <param name="baseUri">The base Uri to call.</param>
+        /// <param name="requestUrl">The request Url.</param>
+        /// <param name="accessToken">The access token (will be discarded).</param>
+        /// <param name="content">The content to post (optional).</param>
+        /// <returns>The http response message.</returns>
+        public async Task<HttpResponseMessage> CallPostAsync(Uri baseUri, string requestUrl, string accessToken, HttpContent content = null)
+        {
+            return await CallPostAsync(baseUri: baseUri, requestUrl: requestUrl, accessToken: accessToken, correlationId: new Guid(), content: content);
+        }
+
+        /// <summary>
+        /// Calls SendRequestAsync() for a POST.
+        /// </summary>
+        /// <param name="baseUri">The base Uri to call.</param>
+        /// <param name="requestUrl">The request Url.</param>
+        /// <param name="accessToken">The access token (will be discarded).</param>
+        /// <param name="correlationId">The CorrelationId</param>
+        /// <param name="content">The content to post (optional).</param>
+        /// <returns>The http response message.</returns>
+        public async Task<HttpResponseMessage> CallPostAsync(Uri baseUri, string requestUrl, string accessToken = null, Guid correlationId = new Guid(), HttpContent content = null)
+        {
+            return await SendRequestAsync(HttpMethod.Post, baseUri: baseUri, requestUrl: requestUrl, correlationId: correlationId, content: content);
+        }
+
+        /// <summary>
+        /// Calls SendRequestAsync() for a POST using the default BaseUri and a blank correlationId.
+        /// </summary>
+        /// <param name="requestUrl">The Request Url.</param>
+        /// <param name="content">The content to post (optional).</param>
+        /// <returns>The http response message.</returns>
+        public async Task<HttpResponseMessage> CallPostAsync(string requestUrl, HttpContent content = null)
+        {
+            return await CallPostAsync(requestUrl, new Guid(), content);
+        }
+
+        /// <summary>
+        /// Calls SendRequestAsync() for a POST using the default BaseUri.
+        /// </summary>
+        /// <param name="requestUrl">The Request Url.</param>
+        /// <param name="correlationId">The CorrelationId</param>
+        /// <param name="content">The content to post (optional).</param>
+        /// <returns>The http response message.</returns>
+        public async Task<HttpResponseMessage> CallPostAsync(string requestUrl, Guid correlationId, HttpContent content = null)
+        {
+            return await SendRequestAsync(HttpMethod.Post, BaseUri, requestUrl, correlationId, content);
+        }
+    }
+}
